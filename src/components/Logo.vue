f3a02547da05b1922c21bbd466f28e995306f822
nattkins <neville.attkins@gmail.com>
2020-03-16 00:21:10 +0000

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,12 +2,12 @@
   <g-link to="/" class="logo" title="Back to home">
     <transition name="theme">
       <div v-if="color == 'dark'">
-        <g-image src="~/assets/img/logobright.svg" alt="logo" />
+        <g-image src="~/assets/img/logoBright.svg" alt="logo" />
       </div>
     </transition>
     <transition name="theme">
       <div v-if="color == 'bright'">
-        <g-image src="~/assets/img/logodark.svg" alt="logo" />
+        <g-image src="~/assets/img/logoDark.svg" alt="logo" />
       </div>
     </transition>
   </g-link>
